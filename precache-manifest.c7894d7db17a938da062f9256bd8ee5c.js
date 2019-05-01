c5ef2ec25c3b44199f76e38e59c4cb857db3bd48
Denis Jeong Plaster <djeong@mhccom.com>
2019-05-01 09:02:34 -0500

    Updates

--- precache-manifest.c7894d7db17a938da062f9256bd8ee5c.js
+++ precache-manifest.c7894d7db17a938da062f9256bd8ee5c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "0a652dd6ca1ddb6a4d2d",
-    "url": "/test-dash/static/css/main.6169eb0e.chunk.css"
+    "revision": "742d59b151ad9ca9ae95",
+    "url": "/test-dash/static/css/main.1e00f629.chunk.css"
   },
   {
-    "revision": "0a652dd6ca1ddb6a4d2d",
-    "url": "/test-dash/static/js/main.df4f5b21.chunk.js"
+    "revision": "742d59b151ad9ca9ae95",
+    "url": "/test-dash/static/js/main.33dabce5.chunk.js"
   },
   {
     "revision": "2c8804b61d24fbb7b6a0",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/test-dash/static/js/2.2427caff.chunk.js"
   },
   {
-    "revision": "5809363fb0bbf2beab4af5888010bc42",
+    "revision": "5b22158775d35fd8b25e96ad9b2c6c8f",
     "url": "/test-dash/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/test-dash/precache-manifest.acba66e41bbb695f3ef7298eaca34d0f.js"
+  "/test-dash/precache-manifest.c7894d7db17a938da062f9256bd8ee5c.js"
 );
 
 workbox.clientsClaim();
